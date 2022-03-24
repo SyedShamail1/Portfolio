a270d5b9e4facd977eac5b24b77bf2038888486a
Syed Shamail <72499162+SyedShamail1@users.noreply.github.com>
2022-03-24 09:53:05 -0700

    Update Intro.jsx

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/Shamail2.png" alt="" />
+          <img src="assets/self.png" alt="" />
         </div>
       </div>
       <div className="right">
